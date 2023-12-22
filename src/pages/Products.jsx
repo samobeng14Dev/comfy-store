9d4966d5155a8e303b8671a3285e3277e4c12a8f
Samuel Obeng <obengsamuel270@gmail.com>
2023-12-22 03:40:43 -0800

    added a pagination feature

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,11 +3,16 @@ import { customFetch } from "../utils";
 
 const url = "/products";
 export const loader = async ({ request }) => {
-	const response = await customFetch(url);
+	// search functionality
+	const params = Object.fromEntries([
+		...new URL(request.url).searchParams.entries(),
+	]);
+	console.log(params);
+	const response = await customFetch(url, { params });
 	const products = response.data.data;
 	const meta = response.data.meta;
 
-	return { products, meta };
+	return { products, meta, params };
 };
 const Products = () => {
 	return (
